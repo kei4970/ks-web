8c6d4f096ca0885dd0dea8ac67f27110d9ce976a
kei4970 <55121356+kei4970@users.noreply.github.com>
2020-05-07 22:12:57 +0900

    과제#2 진행상황 : Dummy 데이터 작성

--- src/main/java/com/sangkon/app/AppApplication.java
+++ src/main/java/com/sangkon/app/AppApplication.java
@@ -1,13 +1,26 @@
 package com.sangkon.app;
 
+import com.sangkon.app.entity.Person;
+import com.sangkon.app.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+	private final PersonRepository personRepository;
+
+	public AppApplication(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		personRepository.save(new Person("김은익", "데이터 분석", "kei4970@naver.com", "010-2466-4970"));
+	}
 }
