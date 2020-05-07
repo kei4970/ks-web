bf3cb192925a59175667af86a78655cabfff57c2
kei4970 <55121356+kei4970@users.noreply.github.com>
2020-05-08 02:49:12 +0900

    과제#2 진행상황 : controller 데이터 연결 계속 오류나는중

--- src/main/java/com/sangkon/app/IndexController.java
+++ src/main/java/com/sangkon/app/IndexController.java
@@ -1,12 +1,22 @@
 package com.sangkon.app;
 
+import com.sangkon.app.repository.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    private final PersonRepository personRepository;
+
+    public IndexController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        Person person = personRepository.findAll().stream().findFirst().orElse(null);
+        model.addAttribute("Person",person);
         return "resume";
     }
 }
